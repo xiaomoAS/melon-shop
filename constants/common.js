65b5192ef881886e641c3ffacc278ff1c8a58566
xiaomoAS <jiangzupei@gmail.com>
2025-09-24 23:51:42 +0800

    feat: 提交接口

--- constants/common.js
+++ constants/common.js
@@ -1,6 +1,20 @@
+// 会员等级
 export const MEMBER_LEVEL = {
   NORMAL: 1, // 普通
   GOLD: 2, // 黄金
   PLATINUM: 3, // 铂金
   DIAMOND: 4, // 钻石
+}
+
+export const MEMBER_LEVEL_NAME = {
+  [MEMBER_LEVEL.NORMAL]: '普通会员',
+  [MEMBER_LEVEL.GOLD]: '黄金会员',
+  [MEMBER_LEVEL.PLATINUM]: '铂金会员',
+  [MEMBER_LEVEL.DIAMOND]: '钻石会员',
+}
+
+// 支付方式
+export const PAY_METHOD = {
+  WE_CHAT: 1, // 微信
+  MEMBER_CARD: 2, // 充值卡
 }
--- pages/settlement/index.vue
+++ pages/settlement/index.vue
@@ -44,28 +44,22 @@
 		</view>
 		<view class="order_coupon_cont">
 			<view class="title_head">
-				<view class="dt ">优惠券</view>
-				<view class="dd">有2张可用</view>
+				<view class="dt">优惠券</view>
+				<view class="dd">有{{ couponList.length }}张可用</view>
 			</view>
 			<view class="det_cont">
-				<view class="dl">
-					<view class="dt">
+				<view v-for="coupon in couponList" :key="coupon.couponId" class="dl">
+					<view v-if="COUPON_TYPE.FREIGHT === coupon.coupon.type" class="dt">
 						<view class="i"><image src="https://melonbamboo.oss-cn-beijing.aliyuncs.com/melonbamboo/05a860ef9f874ed696e19c3374f7419c/order_ico_2.png?Expires=2073876488&OSSAccessKeyId=LTAI5tHrbcXwiX27kw8s1cSb&Signature=5edejPW2awsLyvfjOWNrI8yBClU%3D" mode="widthFix"></image> </view>
 						运费券
 					</view>
-					<view class="dd">
-						-￥8.00 
-						<checkbox value="yfq" color="#61C55E" borderColor="#A0A0A0" activeBackgroundColor="#61C55E" activeBorderColor="#61C55E"  style="transform:scale(0.7)" />
-					</view>
-				</view>
-				<view class="dl">
-					<view class="dt">
+					<view v-else-if="COUPON_TYPE.NEW_DISCOUNT === coupon.coupon.type" class="dt">
 						<view class="i"><image src="https://melonbamboo.oss-cn-beijing.aliyuncs.com/melonbamboo/525a98181c884d0e854f14f241cde3d4/order_ico_3.png?Expires=2073876622&OSSAccessKeyId=LTAI5tHrbcXwiX27kw8s1cSb&Signature=lJjlb4qD0VewJtyVTV5656JOFo8%3D" mode="widthFix"></image> </view>
 						新人专享券
 					</view>
-					<view class="dd">
-						-￥5.00 
-						<checkbox value="yfq" color="#61C55E" borderColor="#A0A0A0" activeBackgroundColor="#61C55E" activeBorderColor="#61C55E"  style="transform:scale(0.7)" />
+					<view  class="dd">
+						<view v-if="coupon.coupon">-￥{{ COUPON_TYPE.NEW_DISCOUNT === coupon.coupon.type ? coupon.coupon.newPersonPrice || 0 : coupon.coupon.waybillPriceLimit || 0 }}</view>
+						<checkbox :checked="coupon.selected" color="#61C55E" borderColor="#A0A0A0" activeBackgroundColor="#61C55E" activeBorderColor="#61C55E"  style="transform:scale(0.7)" />
 					</view>
 				</view>
 			</view>
@@ -93,46 +87,51 @@
 		</view>
 		<view class="order_pay_cont">
 			<view class="title">支付方式</view>
-			<view class="pay_list">
-				<view class="l_cont">
-					<view class="i"><image src="https://melonbamboo.oss-cn-beijing.aliyuncs.com/melonbamboo/7bf717a5de2f4d6d96385b79d7208d9c/order_ico_4.png?Expires=2073953856&OSSAccessKeyId=LTAI5tHrbcXwiX27kw8s1cSb&Signature=hqNrVt4wn%2FQH1AUGdu2DZVnuxtI%3D" mode="widthFix"></image> </view>
-					<view class="cont">
-						<view class="tle">微信支付</view>
+			<radio-group @change="payMethodChange">
+				<view class="pay_list">
+					<view class="l_cont">
+						<view class="i"><image src="https://melonbamboo.oss-cn-beijing.aliyuncs.com/melonbamboo/7bf717a5de2f4d6d96385b79d7208d9c/order_ico_4.png?Expires=2073953856&OSSAccessKeyId=LTAI5tHrbcXwiX27kw8s1cSb&Signature=hqNrVt4wn%2FQH1AUGdu2DZVnuxtI%3D" mode="widthFix"></image> </view>
+						<view class="cont">
+							<view class="tle">微信支付</view>
+						</view>
+					</view>
+					<view class="r_cont">
+						<radio :value="PAY_METHOD.WE_CHAT" :checked="payMethod === PAY_METHOD.WE_CHAT" class="pay-radio" />
 					</view>
 				</view>
-				<view class="r_cont">
-					<checkbox :value="wxpay" style="transform:scale(0.7)" />
-				</view>
-			</view>
-			<view class="pay_list">
-				<view class="l_cont">
-					<view class="i"><image src="https://melonbamboo.oss-cn-beijing.aliyuncs.com/melonbamboo/f96029eafb244211b53a05a59f376bef/order_ico_5.png?Expires=2073953887&OSSAccessKeyId=LTAI5tHrbcXwiX27kw8s1cSb&Signature=LjRrr5ecgpw0kNHxoDxr98iR%2FV4%3D" mode="widthFix"></image> </view>
-					<view class="cont">
-						<view class="tle">会员支付</view>
-						<view class="txt">当前会员等级：黄金会员</view>
-						<view class="txt">账户余额：<text>￥1,250.00</text> </view>
+				<view class="pay_list">
+					<view class="l_cont">
+						<view class="i"><image src="https://melonbamboo.oss-cn-beijing.aliyuncs.com/melonbamboo/f96029eafb244211b53a05a59f376bef/order_ico_5.png?Expires=2073953887&OSSAccessKeyId=LTAI5tHrbcXwiX27kw8s1cSb&Signature=LjRrr5ecgpw0kNHxoDxr98iR%2FV4%3D" mode="widthFix"></image> </view>
+						<view class="cont">
+							<view class="tle">会员支付</view>
+							<view v-if="memberInfo.level" class="txt">当前会员等级：{{ MEMBER_LEVEL_NAME[memberInfo.level] }}</view>
+							<view class="txt">账户余额：<text>￥1,250.00</text> </view>
+						</view>
+					</view>
+					<view class="r_cont">
+						<view class="charge_btn">充值</view>
+						<radio :value="PAY_METHOD.MEMBER_CARD" class="pay-radio" :checked="payMethod === PAY_METHOD.MEMBER_CARD"/>
 					</view>
 				</view>
-				<view class="r_cont">
-					<view class="charge_btn">充值</view>
-					<checkbox :value="hypay" style="transform:scale(0.7)" />
-				</view>
-			</view>
-			<view class="bit_txt">如果您有问题，可以电话联系XXX,我们将高效解决！您还可以在我们未覆盖区域当团长，挣佣金！</view>
+			</radio-group>
+			
+			<!-- TODO: 文案 -->
+			<view class="bit_txt">如果您有问题，可以电话联系XXX,我们将高效解决！</view>
 		</view>
 		<view class="order_foot_cont">
 			<view class="cont">
 				<view class="l_cont">
 					总价: <text>￥{{ priceInfo.totalPrice }}</text>
 				</view>
-				<view class="btn">提交订单</view>
+				<view class="btn" @click="submitHandler">提交订单</view>
 			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-import { MEMBER_LEVEL } from '@/constants/common.js'
+import { MEMBER_LEVEL, MEMBER_LEVEL_NAME, PAY_METHOD } from '@/constants/common.js'
+import { COUPON_TYPE } from '@/components/coupon-list/constants.js'
 
 export default {
 	data() {
@@ -145,10 +144,14 @@ export default {
 			pageOptions: null,
 			productIdList: [],
 			priceInfo: {},
+			payMethod: PAY_METHOD.WE_CHAT,
 			isExpanded: false, // 控制商品列表展开/收起状态
 			maxShowCount: 2, // 初始最多显示的商品数量
 			memberInfo: {},
 			MEMBER_LEVEL,
+			MEMBER_LEVEL_NAME,
+			COUPON_TYPE,
+			PAY_METHOD
 		}
 	},
 	computed: {
@@ -200,6 +203,9 @@ export default {
 			this.getDetail()
 			this.getMemberInfo()
 		},
+		payMethodChange(e) {
+			this.payMethod = Number(e.detail.value)
+		},
 		async getMemberInfo() {
 			try {
 				this.memberInfo = await this.$http.post('/user/member/getUserMember', {})
@@ -231,15 +237,38 @@ export default {
 					// 订单列表-待付款 进来
 				} else if (this.baseProductList.length) {
 					// 购物车进来
-					const { couponList, productList, userAddress } =await this.$http.post('/order/trade', {
+					const { couponList = [], productList = [], userAddress } =await this.$http.post('/order/trade', {
 						tradeProductRequestList: this.baseProductList
 					})
-					this.couponList = couponList
+					this.couponList = couponList.map((item) => ({ ...item, selected: true }))
 					this.productList = productList
 					this.userAddress = userAddress
 				}
 			} catch (error) {
-				
+				this.couponList = []
+				this.productList = []
+				this.userAddress = null
+			}
+		},
+		async submitHandler() {
+			try {
+				if (!this.userAddress) {
+					uni.showToast({ title: '请先选择默认收货地址', icon: 'none' })
+					return
+				}
+				const param = {
+					productList: this.productList.map((item) => ({
+						productId: item.productId,
+						specId: item.specId,
+						buyCounts: item.buyCounts
+					})),
+					addressId: this.userAddress.id,
+					payMethod: this.payMethod,
+					couponIdList: this.couponList.filter((item) => item.selected).map((item) => item.couponId)
+				}
+				await this.$http.post('/order/create', param)
+			} catch (error) {
+				console.log(error)
 			}
 		},
 		// 切换展开/收起状态
